1b4a112c96443ed54381a7549a47182dbd3e113d
Lethiciahas <lethiciahas@gmail.com>
2022-05-27 16:07:48 -0400

    feat: alterações para publicação de projeto em repositorio pessoal conforme manual do estudante da Trybe

--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -5,9 +5,9 @@ class PlanetCard extends Component {
   render() {
     const { planetName, planetImage } = this.props;
     return (
-      <div data-testid="planet-card">
+      <div className="p-card" data-testid="planet-card">
         <p data-testid="planet-name">{planetName}</p>
-        <img src={ planetImage } alt={ `Planeta ${planetName}` } />
+        <img className="img-planet" src={ planetImage } alt={ `Planeta ${planetName}` } />
       </div>
     );
   }
